343b9790805d887e295aa95cb1328ee85bb2b475
Samuel <106215694+Darren2707@users.noreply.github.com>
2023-07-23 19:51:06 +0530

    vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -1,5 +1,4 @@
-# This file is a part of TG-FileStreamBot
-# Coding : Jyothis Jayanth [@EverythingSuckz]
+
 
 import sys
 from os import environ
@@ -10,13 +9,13 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(environ.get("API_ID"))
-    API_HASH = str(environ.get("API_HASH"))
-    BOT_TOKEN = str(environ.get("BOT_TOKEN"))
+    API_ID = int(environ.get("API_ID", "9987125"))
+    API_HASH = str(environ.get("API_HASH" , "db97d01ceffed9938ef4a6297ba2954e"))
+    BOT_TOKEN = str(environ.get("BOT_TOKEN" , "6623257266:AAFPE2uQ2NHAOYi1jSShvMaA-dKDRtOfxVw"))
     SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))  # 1 minte
     WORKERS = int(environ.get("WORKERS", "6"))  # 6 workers = 6 commands at once
     BIN_CHANNEL = int(
-        environ.get("BIN_CHANNEL", None)
+        environ.get("BIN_CHANNEL", "-1001976431041")
     )  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
     PORT = int(environ.get("PORT", 8080))
     BIND_ADDRESS = str(environ.get("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
